b709527c5a0d0e1dbe3e560097d2aa1942378347
jiji1207 <74195274+jiji1207@users.noreply.github.com>
2022-08-11 05:44:59 -0700

    Modify user prompts to restrict non-registered users from deleting files

--- cuc-7.24/login/__init__.py
+++
@@ -1,3 +0,0 @@
-## init.py里面导入pymysql模块
-import pymysql
-pymysql.install_as_MySQLdb()
--- cuc-7.24/login/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-###在admin中注册模型
-
-from . import models
- 
-admin.site.register(models.User)
